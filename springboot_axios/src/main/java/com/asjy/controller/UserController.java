75c9eb5d18d67665eb9d84ad29c1299393e8842f
zwl <1464050900@qq.com>
2023-03-04 09:53:53 +0800

    hot-fix second commit

--- springboot_axios/src/main/java/com/asjy/controller/UserController.java
+++ springboot_axios/src/main/java/com/asjy/controller/UserController.java
@@ -36,6 +36,7 @@ public class UserController {
         System.out.println(3333);
         System.out.println(444);
         System.out.println("hot-fix 修改");
+        System.out.println("hot-fix 第二次修改 冲突问题-----------");
         return R.success(list) ;
     }
 
